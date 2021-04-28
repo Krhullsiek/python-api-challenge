0a891aaae203ba62fb0d02758e6b958745f02667
KALI HULLSIEK <hullsiek@gmail.com>
2021-04-28 13:55:28 -0500

    project complete

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "94fe79ca10aeb1a16010a64e88e6ad028d22bbac"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBobYmwqmoDt_ta7QVT7oWEYz5Rc3Rs1Kw"
